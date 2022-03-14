8715f5415308615314fa083732d9fff3eb6bbf47
Brian Done <briguy2078@gmail.com>
2022-03-14 17:08:12 -0400

    updated portfolio and added project component

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -15,82 +15,73 @@ function Portfolio() {
 				'Handlebars',
 				'Bulma',
 			],
-			github: 'https://github.com/P3T2-Realty-Rogues/living-real',
-			deployed: 'https://living-real.herokuapp.com/',
+			github: 'https://github.com/',
+			deployed: 'herokuapp.com/',
 		},
 		{
-			name: 'Auto Tracker',
+			name: 'What 2 Watch',
 			description:
-				'This is your personal automobile information tracking system. Track time dependent, vehicle specific information from a single application, available on any web-enabled device.',
-			image: 'auto-tracker-image.png',
+				'Movie Database.',
+			image: 'whattowatch.png',
 			technologies: [
 				'HTML/CSS',
-				'JavaScript',
-				'Node.js',
-				'Handlebars.js',
-				'Express & Sequelize',
-				'Heroku & AWS S3',
 			],
-			github: 'https://github.com/OwaisIslam/auto-tracker',
-			deployed: 'https://team-3-auto-tracker.herokuapp.com/',
+			github: 'https://github.com',
+			deployed: 'https://github.com',
 		},
 		{
-			name: 'Flash Beats',
+			name: 'Run Buddy',
 			description:
-				'A clean and accurate way to search for your favorite songs while also displaying the lyrics.',
-			image: 'flash-beats-image.gif',
-			technologies: ['HTML/CSS', 'JavaScript', 'jQuery'],
-			github: 'https://github.com/OwaisIslam/flash-beats',
-			deployed: 'https://teamflash1.github.io/music-app/',
-		},
-		{
-			name: 'Budget Tracker',
-			description:
-				'Budget Tracker is an Application that uses a noSQL database and offline persistance for a great user experience for managing their finances and budgeting no matter where they are.',
-			image: 'budget-tracker-image.png',
-			technologies: [
-				'JavaScript',
-				'MongoDB',
-				'IndexedDB & Service Workers',
-				'Node.js',
-				'Express',
-			],
-			github: 'https://github.com/OwaisIslam/budget-tracker',
-			deployed: 'https://budget-tracker-owais.herokuapp.com/',
-		},
-		{
-			name: 'Weather Dashboard',
-			description:
-				'A weather dashboard that shows current weather and five-day weather forecast for any city.',
-			image: 'weather-dashboard-image.png',
+				'Run Buddy',
+			image: 'runbuddy.png',
 			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com/OwaisIslam/weather-dashboard',
-			deployed: 'https://owaisislam.github.io/weather-dashboard/',
+			github: 'https://github.com/',
+			deployed: 'https://github.com',
 		},
 		{
 			name: 'Password Generator',
 			description:
-				'This is an application that allows you to generate a password from 8 to 128 characters based on criteria you select.',
-			image: 'password-generator-image.png',
-			technologies: ['HTML/CSS', 'JavaScript'],
-			github: 'https://github.com/OwaisIslam/password-generator',
-			deployed: 'https://owaisislam.github.io/password-generator/',
+				'Password Generator',
+			image: 'passwordgenerator.png',
+			technologies: [
+				'JavaScript',
+			],
+			github: 'https://github.com/',
+			deployed: 'https://github.com',
 		},
 		{
-			name: 'Note Taker',
+			name: 'Code Quiz',
 			description:
-				'This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.',
-			image: 'note-taker-image.png',
-			technologies: ['HTML/CSS', 'JavaScript', 'Express.js'],
-			github: 'https://github.com/OwaisIslam/note-taker',
-			deployed: 'https://note-taker-owais.herokuapp.com/',
+				'Code Quiz',
+			image: 'codequiz.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/',
+			deployed: 'https://github.com',
+		},
+		{
+			name: 'Workday Scheduler',
+			description:
+				'Workday Scheduler',
+			image: 'workdayscheduler.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/',
+			deployed: 'https://github.com',
+		},
+		{
+			name: 'Weather Dashboard',
+			description:
+				'Weather Dashboard',
+			image: 'weatherdashboard.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/',
+			deployed: 'https://github.com',
 		},
 	];
 
 	return (
 		<section>
 			<div className="center">
-				<h1 className="page-header">My Portfolio</h1>
+				<h1 className="page-header">Brian Done Portfolio</h1>
 			</div>
 			<div>
 				<ul className="flex-row mobile-row">

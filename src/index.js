2cf000b4a6d3dd6111a22b5a1c84cf8ae358b268
Brian Done <briguy2078@gmail.com>
2023-09-19 17:56:59 -0400

    updated index.js and app.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,17 @@
-import "./index.css";
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-	const [currentTab, setCurrentTab] = useState("about");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-	const renderTab = () => {
-		switch (currentTab) {
-			case "about":
-				return <About />;
-			case "portfolio":
-				return <Portfolio />;
-			case "contact":
-				return <Contact />;
-			case "resume":
-				return <Resume />;
-			default:
-				return null;
-		}
-	};
-
-	return (
-		<div>
-			<div className="mobile-header">
-				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
-			</div>
-			<div>
-				<main>{renderTab()}</main>
-			</div>
-			<div>
-				<Footer></Footer>
-			</div>
-		</div>
-	);
-}
-
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
